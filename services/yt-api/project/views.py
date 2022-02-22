9a561b16d8c3be0c6fdf3363e16cecbdebfff454
saket13 <saketsaumya13@gmail.com>
2022-02-22 23:35:39 +0530

    Search API

--- services/yt-api/project/views.py
+++ services/yt-api/project/views.py
@@ -15,24 +15,42 @@ def fetch_videos():
     }
 
     try:
-        videos = Video.query.paginate(page = page, per_page =limit)
+        videos = Video.query.order_by(Video.publish_time.desc()).paginate(page = page, per_page = limit)
         paginated_videos =(videos.items)
+        results = [video.format() for video in paginated_videos]
     
-        results = [{
-            'video_id':video.video_id,
-            'title':video.title,
-            'description':video.description,
-            'thumbnail' : video.thumbnail,
-            'channel_title' : video.channel_title,
-            'publish_time' : video.publish_time
-        } for video in paginated_videos]
-
         response_dict.update({
             'success' : True,
             'error' : None,
             'results' : results,
             'count' : len(paginated_videos)
         })
+    except Exception as e:
+        response_dict.update({
+            'error' : str(e)
+        })
+
+    return response_dict
+
+@app.route('/search')
+def search_videos():
+
+    query = request.args.get('q', type = str)
+
+    response_dict = {
+        'success' : False,
+        'results' : []    
+    }
+
+    try:
+        videos = Video.query.filter(Video.title.ilike("%{}%".format(query)))
+        results = [video.format() for video in videos]
+
+        response_dict.update({
+            'success' : True,
+            'error' : None,
+            'results' : results
+        })       
     except Exception as e:
         response_dict.update({
             'error' : str(e)
